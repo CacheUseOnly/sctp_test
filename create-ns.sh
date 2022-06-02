442c38ddb8eea4bab7605930c88fdb3a7619db2e
Yuxuan Luo <yuluo@redhat.com>
2022-06-02 12:00:11 -0400

    fixed creat-ns.sh

--- create-ns.sh
+++ create-ns.sh
@@ -19,8 +19,8 @@ sudo ip netns exec 'client' ip link set 'myns-1-eth0' name 'eth0'
 sudo ip netns exec 'server' ip link set 'myns-2-eth0' name 'eth0'
 
 # Assign an address to each interface
-sudo ip netns exec 'client' ip addr add $(CLIENTADDR)/24 dev eth0
-sudo ip netns exec 'server' ip addr add $(SERVERADDR)/24 dev eth0
+sudo ip netns exec 'client' ip addr add $CLIENTADDR/24 dev eth0
+sudo ip netns exec 'server' ip addr add $SERVERADDR/24 dev eth0
 
 # Bring up the interfaces (the veth interfaces the loopback interfaces)
 sudo ip netns exec 'client' ip link set 'lo' up
@@ -29,13 +29,13 @@ sudo ip netns exec 'server' ip link set 'lo' up
 sudo ip netns exec 'server' ip link set 'eth0' up
 
 # Configure routes
-sudo ip netns exec 'client' ip route add default via $(CLIENTADDR) dev eth0
-sudo ip netns exec 'server' ip route add default via $(SERVERADDR) dev eth0
+sudo ip netns exec 'client' ip route add default via $CLIENTADDR dev eth0
+sudo ip netns exec 'server' ip route add default via $SERVERADDR dev eth0
 
 # Support interleave
 sudo ip netns exec 'client' sysctl -w net.sctp.intl_enable=1
 sudo ip netns exec 'server' sysctl -w net.sctp.intl_enable=1
 
 # Test the connection (in both directions)
-sudo ip netns exec 'client' ping -c 2 $(SERVERADDR)
-sudo ip netns exec 'server' ping -c 2 $(CLIENTADDR)
+sudo ip netns exec 'client' ping -c 2 $SERVERADDR
+sudo ip netns exec 'server' ping -c 2 $CLIENTADDR
